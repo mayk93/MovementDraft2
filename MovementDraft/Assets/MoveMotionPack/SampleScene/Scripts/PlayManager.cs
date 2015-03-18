7f681de3846206f73a5840f3c1e35f6e64434d38
mayk93 <mihai.mandrescu@gmail.com>
2015-03-18 13:13:35 +0200

    Refactoring
    Refactoring player machanics.

---
+++ MovementDraft/Assets/MoveMotionPack/SampleScene/Scripts/PlayManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayManager : MonoBehaviour 
+{
+	private Animator player; 
+	private string[] animClipNameGroup;
+	private int currentNumber;
+
+	// Use this for initialization
+	void Start () 
+    {
+		animClipNameGroup = new string[] 
+        {
+			"Basic_Run_02",
+		};
+
+		currentNumber = 0;
+
+        player = GameObject.Find("Player").transform.GetComponentInChildren<Animator>();
+
+        player.speed = 1f;
+        player.Play(animClipNameGroup[currentNumber]);
+	}
+}
--- MovementDraft/Assets/Scripts/PlayerController.cs
+++ MovementDraft/Assets/Scripts/PlayerController.cs
@@ -4,89 +4,94 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour 
 {
-    public float moveSpeed = 10.0f;
-    public float rotationSpeed = 10.0f;
+    public float speed = 6f;            // The speed that the player will move at.
 
-    /* Move to Model */
-    public float scale = 0f;
-    public float health = 100.0f;
-    public Image healthBar;
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Animator anim;                      // Reference to the animator component.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+
+    /* Animations */
+    private Animator player;
+    private string[] animClipNameGroup;
+    private int currentNumber;
+
+    void Awake()
+    {
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask("Floor");
+
+        // Set up references.
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
 
 	// Use this for initialization
-	void Start () {}
+	void Start () 
+    {
+        Animation();
+    }
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
     {
-        Move();
-        CheckLight();
-        CheckHealth();
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h,v);
+        Aim();
 	}
 
     void Aim()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if(Physics.Raycast(ray,out hit))
+        print("Here");
+
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
         {
-            Vector3 mousePosition = new Vector3(ray.GetPoint(hit.distance).x, transform.position.y, ray.GetPoint(hit.distance).z);
-            Vector3 targetDir = mousePosition - transform.position;
-            float step = rotationSpeed * Time.deltaTime;
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-            Debug.DrawRay(transform.position, newDir, Color.red);
-            transform.rotation = Quaternion.LookRotation(newDir);
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            playerRigidbody.MoveRotation(newRotation);
         }
     }
-    void Move()
+    void Move(float h, float v)
     {
-        if (Input.GetKey("w"))
-        {
-            transform.Translate((Vector3.forward) * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("s"))
-        {
-            transform.Translate((Vector3.back) * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.Translate((Vector3.left) * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.Translate((Vector3.right) * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetMouseButton(0))
-        {
-            Aim();
-        }
+        // Set the movement vector based on the axis input.
+        movement.Set(h, 0f, v);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition(transform.position + movement);
     }
 
-    void CheckLight()
+    void Animation()
     {
-        if (Input.GetKeyUp("f"))
+        animClipNameGroup = new string[] 
         {
-            SwitchLight();
-        }
-    }
-    void SwitchLight()
-    {
-        Light light = GetComponentInChildren<Light>();
-        light.enabled = !light.enabled;
-    }
+			"Basic_Run_02",
+		};
 
-    void CheckHealth()
-    {
-        healthBar = GameObject.FindGameObjectWithTag("HealthUI").GetComponent<Image>();
-        healthBar.fillAmount = health / 100.0f;
-    }
+        currentNumber = 0;
 
-    void inflictDamage()
-    {
-        //To Add
-    }
+        player = GameObject.Find("Player").transform.GetComponentInChildren<Animator>();
 
-    public void applyReceivedDamage(float damage)
-    {
-        health = health - damage;
+        player.speed = 1f;
+        player.Play(animClipNameGroup[currentNumber]);
     }
 }
